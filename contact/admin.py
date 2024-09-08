c0a2a9b5e1a64d2354c783b153193089258bc3af
Mateus <160790022+Mateusssssssssss@users.noreply.github.com>
2024-09-08 10:54:58 -0300

    feito a parte dos models e admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from contact import models
+
 # Register your models here.
+#configuração do admin do django
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    ...
+    
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+#blank=True, ex: last_name = models.CharField(max_length=50, blank=True), dai não  obrigatorio colocar o last name
+class Contact(models.Model):
+    firt_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=13)
+    email = models.EmailField(max_length=254)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.firt_name} {self.last_name}'
