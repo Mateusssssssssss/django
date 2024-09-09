21b354797a6f1cf218c275838422c82beae03f02
Mateus <160790022+Mateusssssssssss@users.noreply.github.com>
2024-09-09 16:23:37 -0300

    como os contatos aparecerão na pag do adm
    editando as configs admin

--- contact/admin.py
+++ contact/admin.py
@@ -6,5 +6,16 @@ from contact import models
 #configuração do admin do django
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    #ordem (id, first| name, last name, etc..)
+    list_display = ('id','firt_name', 'last_name', 'phone', 'email',)
+    #ordenaão por id
+    ordering = ('id',)
+    # list_filter = ('created_date',)
+    #cria uma barra de pesquisa
+    search_fields = ('id', 'firt_name', 'last_name',)
+    #numero maximo de contatos a ser exibido na lista
+    list_max_show_all = 100
+    #numero de contactos a serem mostrados por pagina
+    list_per_page = 30
+    
     
--- contact/models.py
+++ contact/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 
-#blank=True, ex: last_name = models.CharField(max_length=50, blank=True), dai não  obrigatorio colocar o last name
+#blank=True, ex: last_name = models.CharField(max_length=50, blank=True), com blank não é obrigatorio colocar o last name
 class Contact(models.Model):
     firt_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=13)
+    phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
