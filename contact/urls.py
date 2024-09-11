08b4bdbfce85ca0113b7ffc5171cb6e7762253b3
Mateus <160790022+Mateusssssssssss@users.noreply.github.com>
2024-09-11 17:57:55 -0300

    algumas mudanças
    criado o arquivo contact.html
    mudado alguns aspctos do css

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,7 @@ from django.urls import path
 app_name ='contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
+    
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,27 @@
 #criado para melhor organização, junto a pasta views
-from django.shortcuts import render
+from ast import If
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+
 
 import contact
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('id')
     
     context = {
         'contacts': contacts, 
     }
     
     return render(request, 'contact/index.html', context)
+
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    
+    context = {
+        'contact': single_contact, 
+    }
+    return render(request, 'contact/contact.html', context )
