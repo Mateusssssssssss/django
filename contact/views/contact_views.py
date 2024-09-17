23d5aa4ed21f945262734f4007dd36719e8b95d2
Mateus <160790022+Mateusssssssssss@users.noreply.github.com>
2024-09-17 09:31:55 -0300

    url e views criada
    criada para o campo pesquisa, para quando digitado o nome, o campo input consiga busca-lo

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,10 @@
 #criado para melhor organização, junto a pasta views
 from ast import If
+import email
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 
 import contact
@@ -12,7 +14,30 @@ def index(request):
     contacts = Contact.objects.filter(show=True).order_by('id')
     
     context = {
-        'contacts': contacts, 
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+         
+    }
+    
+    return render(request, 'contact/index.html', context)
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return redirect('contact:index')
+   
+    contacts = Contact.objects.filter(show=True).filter(Q(firt_name__icontains=search_value) | 
+                                                        Q(last_name__icontains=search_value) |
+                                                        Q(phone__icontains=search_value) |
+                                                        Q(email__icontains=search_value)
+                                                        )\
+                                                            .order_by('id')
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+         
     }
     
     return render(request, 'contact/index.html', context)
@@ -21,7 +46,11 @@ def index(request):
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
     
+    titulo = f'{single_contact.firt_name} {single_contact.last_name} - '
+    
     context = {
-        'contact': single_contact, 
+        'contact': single_contact,
+        'site_title': titulo,
+    
     }
-    return render(request, 'contact/contact.html', context )
+    return render(request, 'contact/contact.html', context)
