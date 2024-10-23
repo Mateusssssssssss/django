f25b963ddb18a0073e86bcc38557a66877019828
Mateus <160790022+Mateusssssssssss@users.noreply.github.com>
2024-10-23 20:28:10 -0300

    validações e ajustes

--- contact/views/forms.py
+++ contact/views/forms.py
@@ -1,28 +1,78 @@
+from cProfile import label
 from django.shortcuts import render, redirect, get_object_or_404
 from contact.models import Contact
 from django import forms
 from django.core.exceptions import ValidationError
 from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
-
-
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 class ContactForm(forms.ModelForm):
+    phone = forms.CharField(
+        validators=[
+            RegexValidator(
+                regex=r'^\d{11}$',  # Aceita apenas números com exatamente 11 dígitos
+                message='O telefone deve conter exatamente 11 dígitos numéricos.'
+            )
+        ],
+        label="Telefone",
+        help_text="Digite o número de telefone com 11 dígitos, incluindo o DDD (ex: 11987654321)."
+    )
     picture = forms.ImageField(
+        label= 'Imagem',
+        help_text= 'Escolha uma imagem para adicionar ao contato (opcional).',
+        required=False,
         widget=forms.FileInput(attrs={
             'accept' : 'image/*',
         })
-    )
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        )
     
     class Meta:
         model = Contact
         fields = ('firt_name', 'last_name', 'phone',
                   'email','description', 'category', 'picture')
+        labels = {
+            'firt_name': 'Nome',
+            'last_name': 'Sobrenome',
+            'phone': 'Telefone',
+            'email': 'E-mail',
+            'description': 'Descrição',
+            'category': 'Categoria',
+        }
         
+        
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        firt_name = cleaned_data.get('firt_name')
+        last_name = cleaned_data.get('last_name')
+
+        if firt_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            
+            msgl = ValidationError(
+                'Segundo nome igual ao Primeiro',
+                code='invalid'
+            )
+            self.add_error('firt_name', msg)
+            self.add_error('last_name', msgl)
+
+        return super().clean()
     
+    def clean_firt_name(self):
+        cleaned_data = self.cleaned_data
+        firt_name = cleaned_data.get('firt_name')
+        msgf = ValidationError('ABC, não é um nome valido', code='invalid')
+        if firt_name == 'ABC':
+            self.add_error('firt_name', msgf)
+            
+        return firt_name
+            
+
+
 
 def create(request):
     form_action = reverse('contact:create')
@@ -86,24 +136,100 @@ def delete(request, contact_id):
         }
     )
 
-
-
-def clean(self):
-        # cleaned_data = self.cleaned_data
-    cleaned_data = self.cleaned_data
-    first_name = cleaned_data.get('first_name')
-    last_name = cleaned_data.get('last_name')
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(required=True, min_length=3)
+    last_name = forms.CharField(required=True, min_length=3)
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email','username', 'password1', 'password2',
+        )
         
-    if first_name == last_name:
-        msg = ValidationError(
-        'Primeiro nome não pode ser igual ao segundo',
-        code='invalid')
-        self.add_error('first_name', msg)
-        self.add_error('last_name', msg)
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email', ValidationError('Já existe este e-mail', code='invalid')
+            )
+        return email
+    
+    
+    
+class RegisterUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Obrigatorio.',
+        error_messages={
+            'min_length': 'Por favor, adicionar pelo menos 2 letras.'
+        }
+    )
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Obrigatorio'
+    )
+
+    password1 = forms.CharField(
+        label="Senha",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "nova-senha"}),
+        required=False,
+    )
+
+    password2 = forms.CharField(
+        label="senha 2",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "nova-senha"}),
+        help_text='Use a mesma senha de antes.',
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username',
+        )
+
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user = super().save(commit=False)
+        password = cleaned_data.get('password1')
+
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
+        return user
+
+    def clean(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error(
+                    'password2',
+                    ValidationError('Senhas devem ser iguais')
+                )
 
         return super().clean()
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        current_email = self.instance.email
 
+        if current_email != email:
+            if User.objects.filter(email=email).exists():
+                self.add_error(
+                    'email',
+                    ValidationError('Já existe este e-mail', code='invalid')
+                )
 
-class RegisterForm(UserCreationForm):
-    ...
+        return email
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import redirect, render
 from contact.views import RegisterForm
 
 
@@ -12,6 +13,8 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuario Registrado')
+            return redirect('contact:index')
     return render(request,'contact/register.html',
                   {
                       'form': form,
